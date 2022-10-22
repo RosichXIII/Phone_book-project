622e04902f80d96b195307ce8a23fb92d42622cc
rosich <rosichxiii@mail.ru>
2022-10-22 22:44:01 +0300

    Запуск из файла Menu. Периодически продолжает запись новых контактов при выборе другого пункта меню...обавлен выбор ввода.

--- Phone_book/Contact.py
+++ Phone_book/Contact.py
@@ -2,10 +2,23 @@ def contact_info():
     import Menu
     import csv
 
-    first_name = input('Введите имя ')
-    family_name = input('Введите фамилию ')   
-    phone = input('Введите номер телефона ')
-    contact = [first_name, family_name, phone]
+    print('\nВыберите режим ввода\n' +
+          '---------------------\n' +
+          'Введите "1" - построчно\n' +
+          'Введите "2" - через пробел\n' +
+          '---------------------\n')
+    input_choice = input('Ввод: ')
+    if input_choice == '1':
+        first_name = input('Введите имя ')
+        family_name = input('Введите фамилию ')   
+        phone = input('Введите номер телефона ')
+        contact = [first_name, family_name, phone]
+    elif input_choice == '2':
+        contact = input('Введите данные через пробел ').split()
+    else:
+        print('Неизвестное значение. Укажите цифру 1 или 2\n')
+        ent = input('Нажмите Enter для продолжения...\n')
+        contact_info()
     with open(Menu.files_names[0], 'a+') as File:        
         File.write(' '.join(contact) + '\n')
     with open(Menu.files_names[1], 'a+') as File:        
